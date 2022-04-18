b72a71262772ade263b53cb1686159bb15cb812c
Melih Onem <melihonem@hotmail.com>
2022-04-18 23:09:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -354,31 +354,6 @@ class TestCommand(Command):
         for cmd_name in self.get_sub_commands():
             self.run_command(cmd_name)
 
-        # Run SelfTest
-        self.announce("running self-tests")
-        old_path = sys.path[:]
-        try:
-            sys.path.insert(0, self.build_dir)
-            from Crypto import SelfTest
-            moduleObj = None
-            if self.module:
-                if self.module.count('.')==0:
-                    # Test a whole a sub-package
-                    full_module = "Crypto.SelfTest." + self.module
-                    module_name = self.module
-                else:
-                    # Test only a module
-                    # Assume only one dot is present
-                    comps = self.module.split('.')
-                    module_name = "test_" + comps[1]
-                    full_module = "Crypto.SelfTest." + comps[0] + "." + module_name
-                # Import sub-package or module
-                moduleObj = __import__( full_module, globals(), locals(), module_name )
-            SelfTest.run(module=moduleObj, verbosity=self.verbose, stream=sys.stdout, config=self.config)
-        finally:
-            # Restore sys.path
-            sys.path[:] = old_path
-
         # Run slower self-tests
         self.announce("running extended self-tests")
 
@@ -396,17 +371,6 @@ kw = {'name':"pycrypto",
                   "Crypto.Random",
                   "Crypto.Random.Fortuna",
                   "Crypto.Random.OSRNG",
-                  "Crypto.SelfTest",
-                  "Crypto.SelfTest.Cipher",
-                  "Crypto.SelfTest.Hash",
-                  "Crypto.SelfTest.Protocol",
-                  "Crypto.SelfTest.PublicKey",
-                  "Crypto.SelfTest.Random",
-                  "Crypto.SelfTest.Random.Fortuna",
-                  "Crypto.SelfTest.Random.OSRNG",
-                  "Crypto.SelfTest.Util",
-                  "Crypto.SelfTest.Signature",
-                  "Crypto.SelfTest.IO",
                   "Crypto.Protocol",
                   "Crypto.PublicKey",
                   "Crypto.Signature",
